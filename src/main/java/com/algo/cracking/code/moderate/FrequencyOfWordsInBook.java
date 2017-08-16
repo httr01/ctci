122200d4ee0a5a594b57f74df7062885303edc7b
Hemant Tanwar <hemant.x.tanwar.-nd@disney.com>
2017-08-15 19:06:31 -0700

    added template for  number to words in book

---
+++ src/main/java/com/algo/cracking/code/moderate/FrequencyOfWordsInBook.java
@@ -0,0 +1,37 @@
+package com.algo.cracking.code.moderate;
+
+import java.io.FileInputStream;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FrequencyOfWordsInBook {
+
+	public int countFreq(String word, String fileWithPath) throws Exception{
+		
+		int CAPACITY = 1000;
+		FileChannel inChannel = new FileInputStream(fileWithPath).getChannel();
+		ByteBuffer buffer = ByteBuffer.allocateDirect(CAPACITY);
+		while(inChannel.read(buffer) > 0){
+			String data = buffer.toString();
+		    buffer.clear(); // do something with the data and clear/compact it.
+		}
+		inChannel.close();
+		return 0;
+	}
+	
+	private int countWordFrequency(String word, String data, Map<String,Integer>  ){
+		
+	}
+	
+	
+	@Test
+	public void test_happyPath1(){
+		 String fileWithPath = "./src/main/resources/book1.txt";
+		 Assert.assertEquals(5, countFreq(fileWithPath));
+		 
+	}
+}
